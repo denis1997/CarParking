3dab70efcfb226ec1bc3381f008a6337597481a3
fedfortu <federico.fortunato@student.univaq.it>
2019-10-29 15:56:36 +0100

    multiple markers

--- src/app/pages/maps/maps.page.ts
+++ src/app/pages/maps/maps.page.ts
@@ -8,21 +8,28 @@ import { Map, latLng, tileLayer, layer, marker} from 'leaflet';
 })
 export class MapsPage {
   map: Map;
+  propertyList = [];
 
   ionViewDidEnter() {
-    this.leafletMap();
-  }
-
-  leafletMap() {
-    this.map = new Map('mapId2').setView([42.5764200, 13.9889900], 13);
+    this.map = new Map('mapId2').setView([42.577655, 13.988621], 13);
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
-    const marks = marker([42.5764200, 13.9889900]).addTo(this.map);
-    }
+    fetch('../../../assets/data.json').then(res => res.json())
+        .then(json => {
+          this.propertyList = json.properties;
+          this.leafletMap();
+        });
+  }
 
+  leafletMap() {
+    for (const property of this.propertyList) {
+      marker([property.long, property.lat]).addTo(this.map)
+          .bindPopup(property.city)
+          .openPopup();
+    }
+    }
   ionViewWillLeave() {
     this.map.remove();
   }
-
 }
